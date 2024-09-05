cf0fb66a58b83bbc0d48cb4555383d5ba3188068
Subhasish Samaddar <subhasish.samaddar@techcrista.com>
2024-09-05 21:01:43 +0530

    full name display ui fix 3

--- src/FormComponent.jsx
+++ src/FormComponent.jsx
@@ -11,34 +11,31 @@ const FormComponent = () => {
   };
 
   return (
-    <div>
-      <div style={{ width: 500, margin: 50 }}>
+    <div style={{ width: 500, margin: 50 }}>
+      <form onSubmit={handleSubmit} style={{ textAlign: "left" }}>
         <h1>Full Name Display</h1>
-        <form onSubmit={handleSubmit}>
-          First Name :
-          <input
-            type="text"
-            onChange={(e) => setFirstName(e.target.value)}
-            placeholder="First Name"
-            required
-          />
-          <br clear="all" />
-          <br clear="all" />
-          Last Name :
-          <input
-            type="text"
-            onChange={(e) => setLastName(e.target.value)}
-            placeholder="Last Name"
-            required
-          />
-          <br clear="all" />
-          <br clear="all" />
-          <button type="submit">Submit</button>
-          <br clear="all" />
-          <br clear="all" />
-          {fullName && `Full Name: ${fullName}`}
-        </form>
-      </div>
+        First Name:
+        <input
+          type="text"
+          onChange={(e) => setFirstName(e.target.value)}
+          placeholder="First Name"
+          required
+        />
+        <br clear="all" />
+        Last Name:
+        <input
+          type="text"
+          onChange={(e) => setLastName(e.target.value)}
+          placeholder="Last Name"
+          required
+        />
+        <br clear="all" />
+        <button type="submit">Submit</button>
+      </form>
+      <br clear="all" />
+      <span style={{ marginLeft: 0 }}>
+        {fullName && `Full Name: ${fullName}`}
+      </span>
     </div>
   );
 };
